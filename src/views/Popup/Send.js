04b02e25cf2149a14b80ee432a432c4475f2d77c
HassanKhan123 <hk2602809@gmail.com>
2022-04-05 11:56:08 +0500

    added: transfer other tokens

--- src/views/Popup/Send.js
+++ src/views/Popup/Send.js
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { CONFIG } from "../../constants";
-import { connect, KeyPair, keyStores } from "near-api-js";
+import { connect, KeyPair, keyStores, transactions } from "near-api-js";
 import { parseNearAmount } from "near-api-js/lib/utils/format";
 import { BN } from "bn.js";
 import { initialTasks } from "../../utils/utilsUpdated";
+
+const FT_TRANSFER_GAS = parseNearAmount("0.00000000003");
+const FT_TRANSFER_DEPOSIT = "1";
 
 const SendTokens = () => {
   const [loading, setLoading] = useState(false);
@@ -44,13 +47,15 @@ const SendTokens = () => {
       if (selectedAsset !== "") {
         let [contractAddress, decimals] = selectedAsset.split(":");
         console.log("OTHER SSETS TRANSFER", contractAddress, receiver);
-        let transfer = await senderAccount.viewFunction(
+        let transfer = await senderAccount.functionCall(
           contractAddress,
           "ft_transfer",
           {
-            receiver_id: receiver,
             amount: (amount * 10 ** decimals).toString(),
-          }
+            receiver_id: receiver,
+          },
+          FT_TRANSFER_GAS,
+          FT_TRANSFER_DEPOSIT
         );
         console.log("HASH========", transfer);
       } else {
